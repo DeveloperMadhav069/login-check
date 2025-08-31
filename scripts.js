d781ec0fd3686a5b4f04dcbfdb861e3d82f96441
Madhav <madhavmehra069@gmail.com>
2025-08-31 21:31:57 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -377,3 +377,31 @@
     });
   });
 })();
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDcwsWKNqy9-sJH5ZjWhkE2Buz4X_Gpk4w",
+  authDomain: "kidpreneur-fc00a.firebaseapp.com",
+  projectId: "kidpreneur-fc00a",
+  storageBucket: "kidpreneur-fc00a.firebasestorage.app",
+  messagingSenderId: "764380304058",
+  appId: "1:764380304058:web:731b533b62ea0d6817e883",
+  measurementId: "G-JRF353FQN2"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+const logoutBtn = document.getElementById('logout-btn');
+if (logoutBtn) {
+  logoutBtn.addEventListener('click', () => {
+    signOut(auth).then(() => {
+      alert('You have been logged out.');
+      window.location.href = 'login.html';
+    }).catch(error => {
+      console.error('Logout error:', error);
+      alert('Error logging out. Please try again.');
+    });
+  });
+}
